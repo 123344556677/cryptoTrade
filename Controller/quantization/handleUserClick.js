649cd5d7278eae79571e672174b2b40ecc3805ad
Bhatti <jordon5611@gmail.com>
2024-07-07 21:15:59 +0500

    Updated getCryptoRated, handleUserClick, and getLevel

--- Controller/quantization/handleUserClick.js
+++ Controller/quantization/handleUserClick.js
@@ -1,3 +1,4 @@
+const { toZonedTime, format } = require('date-fns-tz');
 const Quantization = require('../../models/Quantization');
 const User = require('../../models/User');
 const { NotFoundError, BadRequestError } = require('../../errors');
@@ -23,15 +24,17 @@ const handleUserClick = async (req, res) => {
 
     // Check if a day has passed since the last tap
     const now = new Date();
-    const lastTapDate = new Date(quantization.lastTap);
-    const isSameDay = now.getDate() === lastTapDate.getDate() &&
-        now.getMonth() === lastTapDate.getMonth() &&
-        now.getFullYear() === lastTapDate.getFullYear();
+    const franceTimeZone = 'Europe/Paris';
+    const nowInFrance = toZonedTime(now, franceTimeZone);
+    const lastTapDateInFrance = toZonedTime(new Date(quantization.lastTap), franceTimeZone);
+
+    const isSameDay = format(nowInFrance, 'yyyy-MM-dd') === format(lastTapDateInFrance, 'yyyy-MM-dd');
+
 
     if (!isSameDay) {
         // Reset clicks and lastTap for the new day
         quantization.clicks = 0;
-        quantization.lastTap = now;
+        quantization.lastTap = new Date(Date.now() - 3 * 60 * 1000);
     }
 
     const vipLevelToClicks = {
