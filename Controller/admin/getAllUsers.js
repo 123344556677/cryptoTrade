d4ba14f8c67ef95cbd7e308b313ed5b8078fd2ec
Bhatti <jordon5611@gmail.com>
2024-09-13 20:43:17 +0500

    Created 2 new Api(getUserHistory & getUserTeamContribution) in Admin and updated getAllUsers

--- Controller/admin/getAllUsers.js
+++ Controller/admin/getAllUsers.js
@@ -1,11 +1,28 @@
 const User = require('../../models/User')
+const Quantization = require('../../models/Quantization');
 
 const getAllUsers = async (req, res) => {
 
-    const users = await User.find({role:'user'});
+    const users = await User.find({ role: 'user' });
 
-    res.status(200).json({ status: 'success', users });
-    
+    // Iterate through each user and find their corresponding Quantization document
+    const usersWithVipLevel = await Promise.all(users.map(async (user) => {
+        // Find the quantization for the current user
+        let quantization = await Quantization.findOne({ userId: user._id });
+
+        // If quantization doesn't exist, create a new one
+        if (!quantization) {
+            quantization = new Quantization({ userId: user._id });
+            await quantization.save();
+        }
+
+        // Merge vipLevel with user data
+        return { ...user.toObject(), vipLevel: quantization.vipLevel };
+    }));
+
+
+    res.status(200).json({ status: 'success', users: usersWithVipLevel });
+
 };
 
 module.exports = { getAllUsers }
